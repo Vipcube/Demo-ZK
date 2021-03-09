68832bf190b5b7023f723084c58f58da8f786862
Bread God <Vipcube@users.noreply.github.com>
2021-03-09 11:01:17 +0800

    Let project more clear.

--- Demo-ZK-SpringBoot-JAR/src/main/java/tw/fondus/demo/zk/spring/boot/simple/DemoApplication.java
+++ Demo-ZK-SpringBoot-JAR/src/main/java/tw/fondus/demo/zk/spring/boot/simple/DemoApplication.java
@@ -2,26 +2,11 @@ package tw.fondus.demo.zk.spring.boot.simple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String root() {
-		return "index";
-	}
-	
-	@RequestMapping(value = "/Hello", method = RequestMethod.GET)
-	public String hello() {
-		return "hello";
+		SpringApplication.run( DemoApplication.class, args);
 	}
 }
 
