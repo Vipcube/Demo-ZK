f1967c5a17b4ce47da57e4eb57e179477cf0458d
Bread God <Vipcube@users.noreply.github.com>
2021-03-10 13:54:08 +0800

    Change to basic page and add the index page to control demos.

---
+++ Demo-ZK-MVVM/src/main/java/tw/fondus/demo/zk/spring/boot/mvvm/viewmodel/BasicVM.java
@@ -0,0 +1,44 @@
+package tw.fondus.demo.zk.spring.boot.mvvm.viewmodel;
+
+import lombok.Getter;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.bind.annotation.SmartNotifyChange;
+import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
+import tw.fondus.demo.zk.spring.boot.mvvm.service.ICarService;
+import tw.fondus.demo.zk.spring.boot.mvvm.vo.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@VariableResolver( org.zkoss.zkplus.spring.DelegatingVariableResolver.class )
+public class BasicVM {
+	@WireVariable
+	private ICarService carService;
+
+	@Getter
+	private int count;
+	@Getter
+	private List<Car> model;
+
+	@Init
+	public void init(){
+		this.count = 100;
+		this.model = new ArrayList<>();
+	}
+
+	@Command
+	@SmartNotifyChange( "count" )
+	public void add() {
+		++count;
+	}
+
+	@Command
+	@NotifyChange( "model" )
+	public void search(){
+		this.model.clear();
+		this.model.addAll( this.carService.findAll() );
+	}
+}
